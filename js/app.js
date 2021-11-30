fb7d386f022c3887688712ef0c5241a86d7afe0d
mike <mike@mikedeiMac.local>
2021-11-30 17:11:50 +0800

    feature: add footer

--- js/app.js
+++ js/app.js
@@ -4,3 +4,7 @@ const navLinks = document.querySelector(".nav-links");
 navBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show-links");
 });
+
+const date = document.querySelector("#date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
